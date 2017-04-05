fecf9e9e1792eb7dff1ca8ee6f6025ac9e4d8561
Aaron Lipman <alipman88@gmail.com>
2017-04-05 00:00:18 -0400

    Add simple ReactJS component to home page. ("Hello, World!")

---
+++ app/assets/javascripts/components/app.js.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hello, World!</div>;
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
